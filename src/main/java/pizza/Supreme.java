35807bdb46738eaf4d2e3e9fad2dab5810c49582
Dakshal P <dakshal29@gmail.com>
2023-11-09 12:37:09 -0500

    add temporary constructors to avoid erros in running ui

--- src/main/java/pizza/Supreme.java
+++ src/main/java/pizza/Supreme.java
@@ -1,6 +1,14 @@
 package pizza;
 
+import java.util.ArrayList;
+
 public class Supreme extends Pizza {
+
+    //auto constructor
+    public Supreme(ArrayList<Topping> toppings, Size size, Sauce sauce, boolean extraSauce, boolean extraCheese, double price) {
+        super(toppings, size, sauce, extraSauce, extraCheese, price);
+    }
+
     @Override
     public double price() {
         return 0;
