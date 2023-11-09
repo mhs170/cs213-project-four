f5d8341f2b87301e527445216b5b896cf1b79650
Dakshal P <dakshal29@gmail.com>
2023-11-09 13:23:59 -0500

    add main menu ui

--- src/main/java/pizza/MainApplication.java
+++ src/main/java/pizza/MainApplication.java
@@ -7,14 +7,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader =
-                new FXMLLoader(HelloApplication.class.getResource("hello" +
-                        "-view.fxml"));
+                new FXMLLoader(MainApplication.class.getResource(
+                        "main-menu.fxml"
+                        ));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Main Menu");
         stage.setScene(scene);
         stage.show();
     }
