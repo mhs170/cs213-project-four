4b5008541da165437ee2e7ef2a53e5197671aee3
Dakshal P <dakshal29@gmail.com>
2023-11-17 10:23:21 -0500

    add current order ui

---
+++ src/main/java/pizza/CurrentOrderController.java
@@ -0,0 +1,42 @@
+package pizza;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class CurrentOrderController {
+
+    @FXML
+    private ListView<?> orderList;
+
+    @FXML
+    private TextField orderNumberField;
+
+    @FXML
+    private TextField orderTotalField;
+
+    @FXML
+    private Button placeOrderButton;
+
+    @FXML
+    private Button removePizzaButton;
+
+    @FXML
+    private TextField salesTaxField;
+
+    @FXML
+    private TextField subtotalField;
+
+    @FXML
+    void handlePlaceOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRemovePizza(ActionEvent event) {
+
+    }
+
+}
