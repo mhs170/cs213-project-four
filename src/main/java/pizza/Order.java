6a992972e0daf31eb893b5166bac5b61ddd96dbe
Mohammed Salama <mhs170@scarletmail.rutgers.edu>
2023-11-17 19:38:24 -0500

    Order class

--- src/main/java/pizza/Order.java
+++ src/main/java/pizza/Order.java
@@ -1,4 +1,20 @@
 package pizza;
-
+import java.util.ArrayList;
 public class Order {
+    ArrayList<Pizza> pizzaArray  = new ArrayList<>();
+    private int orderNumber;
+    private static int FIRST_ORDER_NUMBER = 1;
+    public Order(){
+        this.orderNumber = FIRST_ORDER_NUMBER++;
+        this.pizzaArray = new ArrayList<>();
+    }
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+    public void addPizza(Pizza pizza){
+        pizzaArray.add(pizza);
+    }
+    public void removePizza(Pizza pizza){
+        pizzaArray.remove(pizza);
+    }
 }
